07c6e8f6914e621152697b035acc16d62ac86032
qw110946 <810593582@qq.com>
2017-09-06 13:55:49 +0800

    improve index.js   add   以 "(1, 3)" 坐标的方式记录每一步，而不是格子序号 "6"。   在棋步记录列表里加粗显示当前选中的项目。   在 Board 组件中用两个循环渲染出 9 个 Square 格子组件。   添加一个切换按钮来升序或降序显示棋步记录列表。   当一方获胜时，高亮显示连成一线的3颗棋子。

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -3,17 +3,18 @@
     model
 ==========
 */
-/*
- 井字棋模型
+/**
+ * 井字棋模型
  */
-class Tictactoe{
+class Tictactoe {
     /**
      * constructor
      * @param initVar obj
      *      player1 string 玩家1棋子
      *      player2 string 玩家2棋子
      */
-    constructor(initVar) {
+    constructor (initVar) {
+
         this.player1 = initVar.player1;
         this.player2 = initVar.player2;
     }
@@ -21,15 +22,17 @@ class Tictactoe{
    4  * 获取玩家棋子
      * @returns [string,string] player1和player2的棋子
      */
-    getPlayer (){
+    getPlayer () {
+
         return [this.player1, this.player2];
     }
     /**
      * 是否胜利
      * @param squares array 井字棋地图
-     * @returns {boolean}
+     * @returns {boolean}|{array} 胜利会把棋子的位置拼成数组返回
      */
-    calculateWinner(squares){
+    calculateWinner (squares) {
+
         const lines = [
             [0, 1, 2],
             [3, 4, 5],
@@ -40,37 +43,41 @@ class Tictactoe{
             [0, 4, 8],
             [2, 4, 6],
         ];
+
         for (let i = 0; i < lines.length; i++) {
             const [a, b, c] = lines[i];
             if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
                 // return squares[a];
-                return true;
+                return [a,b,c];
             }
         }
+
         return false;
     }
     /**
      * 获取下个玩家
-     * @param isPlayer1Next 下一步的状态 (下一步是否是玩家1下棋)
+     * @param player1IsNext 下一步的状态 (下一步是否是玩家1下棋)
      * @returns {string} 下个玩家的棋子
      */
-    getNextPlayer(isPlayer1Next){
+    getNextPlayer (player1IsNext) {
+
         const [p1,p2] = this.getPlayer();
-        return isPlayer1Next? p1: p2;
+        return player1IsNext? p1: p2;
     }
     /**
      * 获取状态文字
      * @param squares 井字棋
-     * @param isPlayer1Next 下一步的状态 (下一步是否是玩家1下棋)
+     * @param player1IsNext 下一步的状态 (下一步是否是玩家1下棋)
      * @returns {string} 状态文字
      */
-    getStatusText(squares, isPlayer1Next) {
+    getStatusText (squares, player1IsNext) {
+
         const isWinner = this.calculateWinner(squares);
 
         if(isWinner){
-            return 'Winner: ' + this.getNextPlayer(!isPlayer1Next);
+            return 'Winner: ' + this.getNextPlayer(!player1IsNext);
         }else{
-            return 'Next player: ' + this.getNextPlayer(isPlayer1Next);
+            return 'Next player: ' + this.getNextPlayer(player1IsNext);
         }
     }
 }
@@ -79,37 +86,64 @@ class Tictactoe{
     view
 ==========
 */
+/**
+ * 一个棋块
+ * @props
+ *   value string 棋块显示的棋子
+ *   onClick function 棋块点击时触发的事件
+ */
 class Square extends React.Component {
-    render() {
+
+    render () {
+
         return (
             <button className="square" onClick={this.props.onClick}>
-                {this.props.value}
+                {
+                    !this.props.isHighlight?
+                        this.props.value:
+                        <span className="textRed">{this.props.value}</span>
+                }
             </button>
         );
     }
 }
+/**
+ * 整个棋盘
+ * @props
+ *   squares array 棋块的显示的值
+ *   onClick function 每个棋块点击时触发的事件
+ */
 class Board extends React.Component {
-    renderSquare(i) {
+
+    renderSquare (i) {
+
+        const isHighlight = this.props.winnerSquare.indexOf(i) !== -1; // 高亮
+
         return (
             <Square
                 key={i}
                 value={this.props.squares[i]}
                 onClick={() => this.props.onClick(i)}
+                isHighlight={isHighlight}
             />
         );
     }
-    render() {
-        var rowNumber = 3;
-        var colNumber = 3;
-        var rowEls = [];
+    render () {
+
+        const rowNumber = 3;
+        const colNumber = 3;
+        const rowEls = [];
+        var total = 0;
 
         for(let row=0; row<rowNumber; row++){
+
             let colEls = [];
 
             for(let col=0; col<colNumber; col++){
-                console.log((row+1)*(col+1))
-                colEls.push(this.renderSquare((row+1)*col));
+                colEls.push(this.renderSquare(total));
+                total++;
             }
+
             rowEls.push((
                 <div key={row} className="board-row">
                     {colEls}
@@ -124,28 +158,56 @@ class Board extends React.Component {
         );
     }
 }
-class Gamehistory extends React.Component{
-    render(){
+/**
+ * 历史步数
+ * @props
+ *   history array 历史棋盘
+ *   onClick function 每个步骤点击时触发的事件
+ */
+class Gamehistory extends React.Component {
+
+    setText (step, move, stepNumber) {
+
+        // 第0步为game start
+        const text = move ?
+            'Move #' + step.zIndex :
+            'Game start';
+
+        // 为当前步数 加粗
+        return stepNumber === move?<b>{text}</b>:text;
+    }
+
+    render () {
+
         const history = this.props.history;
         const onclick = this.props.onClick;
+        const stepNumber = this.props.stepNumber;
+        const historyStepSort = this.props.historyStepSort;
 
         const els = history.map((step, move) => {
-            const desc = move ?
-                'Move #' + move :
-                'Game start';
+
             return (
                 <li key={move}>
-                    <a href="#" onClick={() => onclick(move)}>{desc}</a>
+                    <a href="#" onClick={() => onclick(move)}>{this.setText(step, move, stepNumber)}</a>
                 </li>
             );
         });
+
+        historyStepSort === 'DESC' && els.reverse();
+
         return(
             <ol>{els}</ol>
         )
     }
 }
+/**
+ * 历史步数
+ * @methods
+ */
 class Game extends React.Component {
-    constructor() {
+
+    constructor () {
+
         super();
 
         // 设置初始棋盘对象
@@ -158,59 +220,115 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null),
             }],
-            player1IsNext: true, // 玩家一先下棋
-            stepNumber: 0, // 第零步
+            player1IsNext: true, // 是否玩家一先下棋
+            stepNumber: 0, // 步数
+            historyStepSort: 'ASC', // 历史步数显示顺序
         };
     }
-    handleClick(i) {
+    /**
+     * 棋块点击事件
+     * @param i 被点击的方块
+     */
+    handleClick (i) {
+
         const history = this.state.history.slice(0,this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
-        const nextPlay = this.state.xIsNext;
+        const player1IsNext = this.state.player1IsNext;
         const tict = this.tictactoe;
 
         // 有一方获胜 或 已经有棋子下落
         if (tict.calculateWinner(squares) || squares[i]){
             return
         }
+
         // 下个棋子
-        squares[i] = tict.getNextPlayer(nextPlay);
+        squares[i] = tict.getNextPlayer(player1IsNext);
+
+        // 计算位置
+        const row = i/3 >= 2? '3':
+            i/3 >= 1?
+                '2':
+                '1';
+        const col = (i=>{
+            var  = i/3;
+            col = parseInt((col-parseInt(col))*100)/100
+            return col === 0.66? '3':
+                col === 0.33?
+                    '2':
+                    '1';
+        })(i);
+        const zIndex = '(' + row + ',' + col + ')';
 
         this.setState({
             history: history.concat([{
-                squares: squares
+                squares: squares,
+                zIndex: zIndex
             }]),
             stepNumber: history.length,
-            xIsNext: !this.state.xIsNext
+            player1IsNext: !player1IsNext
         });
     }
-    jumpTo(step) {
+    /**
+     * 步骤点击事件
+     * @param step 被点击的的步数
+     */
+    jumpTo (step) {
+
         this.setState({
             stepNumber: step,
-            xIsNext: (step % 2) ? false : true,
+            player1IsNext: (step % 2) ? false : true,
+        });
+    }
+    /**
+     * 改变排序方式
+     */
+    switchHistoryStepSortType (){
+
+        this.setState({
+            historyStepSort: this.state.historyStepSort === 'ASC'?'DESC':'ASC'
         });
     }
     render() {
+
         const history = this.state.history;
-        const current = history[this.state.stepNumber];
+        const stepNumber = this.state.stepNumber;
+        const current = history[stepNumber];
         const tict = this.tictactoe;
-        const nextPlay = this.state.xIsNext;
+        const player1IsNext = this.state.player1IsNext;
+        const historyStepSort = this.state.historyStepSort;
+
+
+        // 是否胜利
+        const isWinner = tict.calculateWinner(current.squares);
+        // 胜利的三个棋子下标
+        var winnerSquare = [];
+
+        if(isWinner){
+            winnerSquare = isWinner;
+        }
 
         // 状态信息
-        let statusText = tict.getStatusText(current.squares, nextPlay);
+        const statusText = tict.getStatusText(current.squares, player1IsNext);
 
         return (
             <div className="game">
                 <div className="game-board">
                     <Board
                         squares={current.squares}
-                        onClick={(i) => this.handleClick(i)}/>
+                        onClick={(i) => this.handleClick(i)}
+                        winnerSquare={winnerSquare}/>
                 </div>
                 <div className="game-info">
                     <div>{statusText}</div>
+                    <div>sort：<button
+                            className="historyStepSort"
+                            onClick={()=>this.switchHistoryStepSortType()}>{historyStepSort}</button></div>
                     <Gamehistory
                         history={history}
-                        onClick={(step) => this.jumpTo(step)}/>
+                        onClick={(step) => this.jumpTo(step)}
+                        stepNumber={stepNumber}
+                        historyStepSort={historyStepSort}/>
                 </div>
             </div>
         );
