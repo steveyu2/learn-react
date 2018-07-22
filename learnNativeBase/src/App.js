e88260762726ff59cf35c15b1e43fe5b70741108
qw110946 <810593582@qq.com>
2018-07-22 11:55:06 +0800

    更新 native-base

--- learnNativeBase/src/App.js
+++ learnNativeBase/src/App.js
@@ -1,47 +1,22 @@
 import React, { Component } from 'react';
-import { Container, Header, Button, Icon, Fab } from 'native-base';
-import { createAnimatableComponent, View } from 'react-native-animatable';
-
-export default class FABExample extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      active: false
-    };
-  }
-  render() {
-    return (
-      <Container>
-        <Header />
-        <View style={{ flex: 1 }}>
-          <Fab
-            active={ this.state.active }
-            direction="left"
-            containerStyle={{ }}
-            style={{ backgroundColor: '#5067FF' }}
-            position="bottomRight"
-            onPress={() => this.setState({ active: !this.state.active })}>
-            <Icon name="share" />
-              <Button>
-                <View animation="tada"  style={{
-                  height: '100%',
-                  width: '100%',
-                  borderRadius: 50,
-                  alignItems: 'center',
-                  backgroundColor: '#34A34F' }} delay={1000}>
-                  <Icon name="logo-whatsapp" />
-                </View>
-              </Button>
-            <Button style={{ backgroundColor: '#3B5998' }}>
-              <Icon name="logo-facebook" />
-            </Button>
-            <Button disabled style={{ backgroundColor: '#DD5144' }}>
-              <Icon name="mail" />
-            </Button>
-          </Fab>
-        </View>
-      </Container>
-    );
-  }
+import { Drawer, Text, View } from 'native-base';
+export default class DrawerExample extends Component {
+    render() {
+        closeDrawer = () => {
+            this.drawer._root.close()
+        };
+        openDrawer = () => {
+            this.drawer._root.open()
+        };
+        return (
+                <Drawer
+                    ref={(ref) => { this.drawer = ref; }}
+                    content={<View style={{ height: '120%',width:'50%',backgroundColor:'blue' }}><Text>123</Text></View>}
+                    onClose={() => closeDrawer()} >
+                    <View style={{ flex: 1 }}>
+                            <Text>123123</Text>
+                    </View>
+                </Drawer>
+        );
+    }
 }
